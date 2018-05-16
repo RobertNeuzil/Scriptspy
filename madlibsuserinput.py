4f35ef98958995f2632b70e6d4d01e5acfe4946b
Robert Neuzil <robertneuzil@gmail.com>
2018-05-16 19:29:50 -0400

    madlibs
    the children's game madlibs

---
+++ madlibsuserinput.py
@@ -0,0 +1,19 @@
+adjective = input ("please enter an adjective:")
+adjectivetwo = input("please enter another adjective:")
+adjectivethree = input("please enter another adjective:")
+color = input("please enter a color:")
+verb = input("please enter a verb:")
+place = input("please enter a place:")
+planet = input("please enter a planet:")
+task = input("please enter a task:")
+number = input("please enter a number:")
+	
+print (f"""
+The meatballs at our school are very {adjective}. They look very {color} and taste very 
+{adjectivetwo}. Last week, I swore I saw one get up and {verb}! They must come from {place}.
+If I didn't know better, I would say they were from {planet}! Before long, one of them is going to learn how
+to {task}. Then we will all be in trouble! I give it less than {int(number)} days.
+
+
+""")
+	
