a767105135bf712a1dd1f502cb9c588c447cfbb6
Robert Neuzil <robertneuzil@gmail.com>
2018-05-17 12:06:39 -0400

    listcompt etc
    update list comp 5/17/18

--- listcomp.py
+++ listcomp.py
@@ -13,5 +13,36 @@ print (mylist)
 mylist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] # creat new list with even numbers
 mylist = [n for n in mylist if n % 2 == 0] #repeating line 14
 print (mylist)
+
+
 mylist = [(letter, num) for letter in "xyz" for num in mylist] #loops through both all at once. All possible combos
 print (mylist)
+
+first = ["Allan", "Alex", "Jason", "Allison"]
+last = ["White", "West", "Turner", "Lanson"]
+
+together = {first : last for first, last in zip (first, last) if first != "Allan"}
+print (together) 
+
+
+numset = [1,2,3,4,5,6,7,8,8,8,8,8,8,8,8,8,8,9]
+
+################################################################################## generator expressions
+
+numgen = [1,2,3,4,5,6,7,8,9,10]
+
+numgen = (n*n for n in numgen)
+for n in numgen:
+	print (n)
+
+ 
+
+
+ 
+numset = {n for n in numset} # in order to get only unique values (no repeating) you can use curly braces
+print (numset)
+
+
+
+
+
