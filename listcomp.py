e9bf327367a84bfd51e4c6d23232b7a3641e70a6
Robert Neuzil <robertneuzil@gmail.com>
2018-05-17 10:01:00 -0400

    5/17/18
    Updates list comprehension.

--- listcomp.py
+++ listcomp.py
@@ -8,3 +8,10 @@ mylist = [n + 2 + n + 13 for n in mylist] # iterates through list and adds two t
 print (mylist)
 mylist = [n for n in range(1, 3) for n in mylist] #creates new list takes the previous list and loops through it twice range(1, 3) 
 print (mylist)
+mylist = [n for n in mylist if (n % 2 == 0)] #displays all even numbers in list. Returns blank list in this case.
+print (mylist)
+mylist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] # creat new list with even numbers
+mylist = [n for n in mylist if n % 2 == 0] #repeating line 14
+print (mylist)
+mylist = [(letter, num) for letter in "xyz" for num in mylist] #loops through both all at once. All possible combos
+print (mylist)
