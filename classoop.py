e1ff087d2c4dca32e204daefba436551e7d2042b
Robert Neuzil <robertneuzil@gmail.com>
2018-05-18 11:43:54 -0400

    5/18/18
    Class and static methods added.

--- classoop.py
+++ classoop.py
@@ -9,21 +9,23 @@ class Dog():
 		self.treats = self.treats + 1
 
 	@classmethod 
-	def set_class_amount(cls, treats, animals):
-		cls.treats = treats 
-		cls.animals = animals
+	def classassign(cls, house, toy):
+		cls.house = house 
+		cls.toy = toy
 
-Dog.set_class_amount(333, "dogs") # class methods effects the whole class as opposed to each instance
-xyz = Dog("Rodrigo", "Doberman", "White")
-lol = Dog("Crayon", "Boxer", "Brown")
-nvm = Dog("Scooter", "Pitbull", "White")
-abc = Dog("Spot", "Pitbull", "White")
-print (xyz.treats)
-xyz.do_trick()
-print (xyz.treats)
-print (lol.treats)
-print (nvm.treats)
-print (abc.treats)
+	@staticmethod
+	def barkbark ():
+		if Dog.toy == True:
+			print ("Woof Woof")
+		else:
+			pass
+
+
+Dog.classassign(False, True)
+lol = Dog("Roscoe", "Doberman", "White")
+print (lol.house)
+lol.barkbark()
+
 
 
 
