f7733578b7006bf4877f03fdd2e8b1d395f14b20
Robert Neuzil <robertneuzil@gmail.com>
2018-05-20 00:36:11 -0400

    5/20/18
    Updated and documented web scraper to add titles of page to write to hard drive in text format

--- webscraper.py
+++ webscraper.py
@@ -2,12 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 x = 1
 y = 120
-while x <= 10:
-	url = "https://jacksonville.craigslist.org/search/cta?s=" + str(y) 
-	source = requests.get(url)
-	text_data = source.text 
-	soup = BeautifulSoup(text_data)
-	for title in soup.find_all(class_="result-title hdrlnk"):
-		print (title.text)
+while x <= 10:   #loops through more than one page
+	url = "https://jacksonville.craigslist.org/search/boo?s=" + str(y) #adds new pages to url 
+	source = requests.get(url) #pulls html
+	text_data = source.text  # readable formate
+	soup = BeautifulSoup(text_data) # BS OBJECT
+	for title in soup.find_all(class_="result-title hdrlnk"): # using soup object to find titles in html class
+		f = open("namelist.txt","a") # create new file
+		f.write(title.text) # put titles on hard drive in text format
+		f.close() 
 	x += 1
 	y += 120
