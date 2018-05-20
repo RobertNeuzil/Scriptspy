f4716b42d5f331937ff3f175df6a97ed9a591e71
Robert Neuzil <robertneuzil@gmail.com>
2018-05-20 17:59:03 -0400

    Updates webscraper.
    Pulls list of SFW subreddits from reddit list. Updated accompanying text file.

--- webscraper.py
+++ webscraper.py
@@ -1,15 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 x = 1
-y = 120
+y = 1
 while x <= 10:   #loops through more than one page
-	url = "https://jacksonville.craigslist.org/search/boo?s=" + str(y) #adds new pages to url 
+	url = "http://redditlist.com/?page=" + str(y) #adds new pages to url 
 	source = requests.get(url) #pulls html
-	text_data = source.text  # readable formate
+	text_data = source.text  # readable format
 	soup = BeautifulSoup(text_data, "html.parser") # BS OBJECT
-	for title in soup.find_all(class_="result-title hdrlnk"): # using soup object to find titles in html class
+	for title in soup.find_all(class_ = "sfw"): # using soup object to find titles in html class
 		f = open("namelist.txt","a") # create new file
 		f.write(title.text) # put titles on hard drive in text format
-		f.close() 
+		f.close()
 	x += 1
-	y += 120
+	y += 1
