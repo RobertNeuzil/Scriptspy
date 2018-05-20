36f8a6c9254d396d92ff03cf3001db9df408ea59
Robert Neuzil <robertneuzil@gmail.com>
2018-05-20 00:46:07 -0400

    Updated webscraper parser
    5/20/18

--- webscraper.py
+++ webscraper.py
@@ -6,7 +6,7 @@ while x <= 10:   #loops through more than one page
 	url = "https://jacksonville.craigslist.org/search/boo?s=" + str(y) #adds new pages to url 
 	source = requests.get(url) #pulls html
 	text_data = source.text  # readable formate
-	soup = BeautifulSoup(text_data) # BS OBJECT
+	soup = BeautifulSoup(text_data, "html.parser") # BS OBJECT
 	for title in soup.find_all(class_="result-title hdrlnk"): # using soup object to find titles in html class
 		f = open("namelist.txt","a") # create new file
 		f.write(title.text) # put titles on hard drive in text format
