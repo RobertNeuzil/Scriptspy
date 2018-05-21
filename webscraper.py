44a3cd164946b0f9b51f299b1c2f8568473f284a
Robert Neuzil <robertneuzil@gmail.com>
2018-05-20 21:27:55 -0400

    Webscraper update
    5/20/18

--- webscraper.py
+++ webscraper.py
@@ -1,15 +1,15 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+
+
 x = 1
-y = 1
-while x <= 10:   #loops through more than one page
-	url = "http://redditlist.com/?page=" + str(y) #adds new pages to url 
-	source = requests.get(url) #pulls html
-	text_data = source.text  # readable format
-	soup = BeautifulSoup(text_data, "html.parser") # BS OBJECT
-	for title in soup.find_all(class_ = "sfw"): # using soup object to find titles in html class
-		f = open("namelist.txt","a") # create new file
-		f.write(title.text) # put titles on hard drive in text format
+while x < 20:
+	url = "http://redditlist.com/?page=" + str(x)
+	alldata = requests.get(url)
+	text = alldata.text
+	soup = BeautifulSoup(text, "html.parser")
+	for titles in soup.find_all(class_="sfw"):
+		f = open("namelist.txt", "a")
+		f.write(titles.text)
 		f.close()
 	x += 1
-	y += 1
