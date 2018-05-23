b520eeab55797df264d06f8a42099713930eeba6
Robert Neuzil <robertneuzil@gmail.com>
2018-05-23 01:49:23 -0400

    rock paper scissors only strings
    previously was comparing ints and assigning print statements based on two of same ints. Now only accepts strings as inputs.

--- practicepython.org challenges/exeight.py
+++ practicepython.org challenges/exeight.py
@@ -1,26 +1,29 @@
 def playgame():  # creating a function to call game
-
+	
 	playerone = input("Please enter rock, paper or scissors:")
 	playertwo = input("Please enter rock, paper or scissors:")
 	playerone.lower() # makes it simpler to compare strings in variables
 	playertwo.lower()
-
-	if playerone == playertwo: 
+	
+	if str(playerone) == str(playertwo): 
 		print ("tie game")
-	elif playerone == "rock": # player one entered rock
-		if playertwo == "scissors":
+	elif str(playerone) == "rock": # player one entered rock
+		if str(playertwo) == "scissors":
 			print ("rock wins")  
-		elif playertwo == "paper":    # wins or loses, according to game rules
+		elif str(playertwo) == "paper":    # wins or loses, according to game rules
 			print ("paper wins")
-	elif playerone == "paper":   # player one entered paper
-		if playertwo == "rock":
+	elif str(playerone) == "paper":   # player one entered paper
+		if str(playertwo) == "rock":
 			print ("paper wins")
-		if playertwo == "scissors":
+		if str(playertwo) == "scissors":
 			print ("scissors wins")
-	elif playerone == "scissors":
-		if playertwo == "rock":
+	elif str(playerone) == "scissors":
+		if str(playertwo) == "rock":
 			print ("rock wins")
-		if playertwo == "paper":
+		if str(playertwo) == "paper":
 			print ("scissors wins")
+	else:
+		print ("your entry was not valid. Please play again")
+
 # no loop needs to be created for player two. He/She are already accounted for in their possible
 # choices within the if/else for player one. 
