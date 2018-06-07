f26d30980a5998fa7bc0ebf361e1a391e0738180
Robert Neuzil <robertneuzil@gmail.com>
2018-06-06 21:53:21 -0400

    updates first class functions

--- firstclassfunctions.py
+++ firstclassfunctions.py
@@ -1,16 +1,25 @@
-def square(num):
-	return num * num
 
-def my_map(func, arg_list):
-	result = []
-	for i in arg_list:
-		result.append(func(i))
-	return result
+def hello(x):      #create a function
+	return x * x # returns a square
 
-squares_fcf = my_map(square, [3, 9, 11, 20, 49])
+def goodbye (func, args_list):  #takes the argument of function and a list
+	new = [] # will return new list after
+	for i in args_list: # loop through every number in arg_list
+		new.append(func(i)) # append it to new after the numbers in arg list are run through the func
+	return new
 
-print (squares_fcf)
+squares = goodbye(hello, [1, 2, 3, 4, 5]) 
+print (squares)
 
 
 
+def trythis(x):
+
+	def text_wrapper(y):
+		print (f"{x} {y} {y} {x}")
+	return text_wrapper
+
+something = trythis("Something") #something has now been committed to memory to keep the "Something" argument for try this over and over again
+something("middle")
+something("car")
 
