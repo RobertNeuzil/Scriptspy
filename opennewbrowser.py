785ddc9579fe78d672988fbb9280ea0a976a2f92
Robert Neuzil <robertneuzil@gmail.com>
2018-08-22 12:33:38 -0400

    second webbrowser script

---
+++ opennewbrowser.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+webbrowser.open_new('www.python.org')
+webbrowser.open_new('www.reddit.com')
+webbrowser.open_new('www.facebook.com')
+webbrowser.open_new('www.instagram.com')
+webbrowser.open_new('www.youtube.com')
