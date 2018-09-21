841a921bf60b5b665af6faa8539d68a27649a533
Robert Neuzil <robertneuzil@gmail.com>
2018-09-21 18:30:12 -0400

    first class function clarity

--- firstclassfunctions.py
+++ firstclassfunctions.py
@@ -1,51 +1,58 @@
-
-def hello(x):      #create a function
-	return x * x # returns a square
-
-def goodbye (func, args_list):  #takes the argument of function and a list
-	new = [] # will return new list after
-	for i in args_list: # loop through every number in arg_list
-		new.append(func(i)) # append it to new after the numbers in arg list are run through the func
-	return new
-
-squares = goodbye(hello, [1, 2, 3, 4, 5]) 
-print (squares)
+"""
+functions can be passed into other functions in order to carry out multiple operations at once """
 
 
+def squared(f):
+	return f * f
 
-def trythis(x):
+lol = squared
 
-	def text_wrapper(y):
-		print (f"{x} {y} {y} {x}")
-	return text_wrapper
+def cubed (f):
 
-something = trythis("Something") #something has now been committed to memory to keep the "Something" argument for try this over and over again
-something("middle")
-something("car")
+	return f * f * f
 
+def subtract (f):
 
-######################################################################################################decorators######
+	return f - 3
 
+rofl = subtract
 
-def decorator(f):  # the decorator needs an implicit argument that will serve as the function you're calling in the nested wrapper function
-
-	def wrapper(*args):
-		print ("Before function called")
-		f(*args)
-		print ("After function called")
-	return wrapper # no parentheses because that would execute function. Only looking to return the wrapper function
-	#within the decorator in order to later pass in f() and then execute wrapper + f() together, at once. They will all be
-	# executed when the decorated function is called
+def mapping(myfunc, my_list):
+	result = []
+	for x in my_list:
+		result.append(myfunc(x))
+	return result
 
 
-@decorator
-def printname(name):
-	print (name)
-
-printname("Robert")
+print (mapping(lol, [5, 10, 15, 100, 105]) )
+print (mapping(cubed, [5, 10, 15, 100, 105]))
+print (mapping(rofl, [5, 10, 15, 100, 105]))
 
 
 
+
+
+
+def logger(msg):
+
+	def printlog():
+		print ("log:" + msg)
+
+	return printlog
+
+hitherelog = logger(" Hi")
+hitherelog()
+
+
+def html_tag(tag):
+
+	def write(msg):
+		return f'{tag}{msg}{tag}'
+
+	return write
+
+hello_world = html_tag(' </h1> ')
+print (hello_world("Hello World"))
 
 
 
