004ca8446c4fcb04cf21336a96a0c6a7abbdb89b
Robert Neuzil <robertneuzil@gmail.com>
2018-09-22 13:08:25 -0400

    decorators

--- decorators.py
+++ decorators.py
@@ -1,12 +1,22 @@
-def outer_func(msg):
+def decorator_func(my_func_test):
+	
+	def wrapper_func():
+		
+		return my_func_test
 
-	def inner_func(msgtwo):
-		print (msg + msgtwo)
-	return inner_func
+	return wrapper_func
 
-grand = outer_func("Hi")
-grand(" and good morning")
-grand(" and good night")
-grand(" and good evening")
-grand(" and it's lunchtime")
-grand(" and it's 5 pm")
+def test():
+
+	print ("The test worked!")
+
+def test_again():
+	
+	print("The test worked again")
+
+
+first = test
+second = test_again
+
+first()
+second()
