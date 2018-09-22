c130efcba58e7d618458093163cee60c043c52dd
Robert Neuzil <robertneuzil@gmail.com>
2018-09-22 19:12:35 -0400

    more os and files

--- fileobjects.py
+++ fileobjects.py
@@ -1,4 +1,9 @@
-with open("test.txt", "r") as f:
+with open("loremipsumdummy.txt", "r") as f:
+	size_to_read = 50
+	f_contents = f.read(size_to_read)
+	while len(f_contents) > 0:
+		print (f_contents, end='^&*')
+		f_contents = f.read(size_to_read)
+		
+		
 
-	f_contents = f.read()
-	print (f_contents)
