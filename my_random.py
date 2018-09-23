661ab2ea8970980cb258cff6e5d563ca634ffbae
Robert Neuzil <robertneuzil@gmail.com>
2018-09-23 14:56:21 -0400

    real life scenarios represented with random module

--- my_random.py
+++ my_random.py
@@ -1,5 +1,5 @@
 import random
-
+"""
 def roll_of_dice():
 	roll = random.randint(0, 6)
 	if roll == 1:
@@ -14,7 +14,40 @@ def roll_of_dice():
 		print ("The dice came up five")
 	else:
 		print ("The dice is a six")
+
 roll_of_dice()
 roll_of_dice()
 roll_of_dice()
 roll_of_dice()
+"""
+
+
+"""
+def choose_a_greeting():
+	greeting = ['Hello', 'Hola', "Heeeyyyy", "Hi", "Konnichiwa"]
+	which_greeting = random.choice(greeting)
+	print (which_greeting)
+
+choose_a_greeting()
+choose_a_greeting()
+choose_a_greeting()
+"""
+
+"""
+def roll_of_dice():
+	dice = [1, 2, 3, 4, 5, 6]
+	choose = random.choices(dice, k=1)
+	print (f' Your dice roll was a {choose}')
+
+roll_of_dice()
+roll_of_dice()
+roll_of_dice()
+roll_of_dice()
+"""
+
+
+def roulette_wheel():
+	colors = ['red', 'black', 'green']
+	choose = random.choices(colors, weights= [18, 18, 2], k=10)
+	print (choose)
+roulette_wheel()
